a8a6ba2f7c892cfe788cab163f3ca51ecbff1f4a
Panzerhandschuh <TylerBeff@gmail.com>
2014-05-30 10:49:38 -0400

    Initial commit

---
+++ gamemode/init.lua
@@ -0,0 +1,113 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+DEFINE_BASECLASS( "gamemode_base" )
+
+local MAX_AMMO = {}
+MAX_AMMO["Pistol"] = 150
+MAX_AMMO["357"] = 12
+MAX_AMMO["SMG1"] = 225
+MAX_AMMO["SMG1_Grenade"] = 3
+MAX_AMMO["AR2"] = 60
+MAX_AMMO["AR2AltFire"] = 3
+MAX_AMMO["Buckshot"] = 30
+MAX_AMMO["XBowBolt"] = 10
+MAX_AMMO["RPG_Round"] = 3
+MAX_AMMO["Grenade"] = 5
+
+local ITEM_TO_AMMO = {}
+ITEM_TO_AMMO["item_ammo_357"] = "357"
+ITEM_TO_AMMO["item_ammo_357_large"] = "357"
+ITEM_TO_AMMO["item_ammo_ar2"] = "AR2"
+ITEM_TO_AMMO["item_ammo_ar2_large"] = "AR2"
+ITEM_TO_AMMO["item_ammo_ar2_altfire"] = "AR2AltFire"
+ITEM_TO_AMMO["item_ammo_crossbow"] = "XBowBolt"
+ITEM_TO_AMMO["item_ammo_pistol"] = "Pistol"
+ITEM_TO_AMMO["item_ammo_pistol_large"] = "Pistol"
+ITEM_TO_AMMO["item_ammo_smg1"] = "SMG1"
+ITEM_TO_AMMO["item_ammo_smg1_large"] = "SMG1"
+ITEM_TO_AMMO["item_ammo_smg1_grenade"] = "SMG1_Grenade"
+ITEM_TO_AMMO["item_box_buckshot"] = "Buckshot"
+ITEM_TO_AMMO["item_rpg_round"] = "RPG_Round"
+
+local WEP_TO_AMMO = {}
+WEP_TO_AMMO["weapon_357"] = "357"
+WEP_TO_AMMO["weapon_ar2"] = "AR2"
+WEP_TO_AMMO["weapon_crossbow"] = "XBowBolt"
+WEP_TO_AMMO["weapon_pistol"] = "Pistol"
+WEP_TO_AMMO["weapon_rpg"] = "RPG_Round"
+WEP_TO_AMMO["weapon_shotgun"] = "Buckshot"
+WEP_TO_AMMO["weapon_smg1"] = "SMG1"
+WEP_TO_AMMO["weapon_frag"] = "Grenade"
+
+local ITEM_TO_AMOUNT = {}
+ITEM_TO_AMOUNT["item_ammo_357"] = 6
+ITEM_TO_AMOUNT["item_ammo_357_large"] = 12
+ITEM_TO_AMOUNT["item_ammo_ar2"] = 20
+ITEM_TO_AMOUNT["item_ammo_ar2_large"] = 60
+ITEM_TO_AMOUNT["item_ammo_ar2_altfire"] = 1
+ITEM_TO_AMOUNT["item_ammo_crossbow"] = 6
+ITEM_TO_AMOUNT["item_ammo_pistol"] = 20
+ITEM_TO_AMOUNT["item_ammo_pistol_large"] = 100
+ITEM_TO_AMOUNT["item_ammo_smg1"] = 45
+ITEM_TO_AMOUNT["item_ammo_smg1_large"] = 225
+ITEM_TO_AMOUNT["item_ammo_smg1_grenade"] = 1
+ITEM_TO_AMOUNT["item_box_buckshot"] = 20
+ITEM_TO_AMOUNT["item_rpg_round"] = 1
+
+local WEP_CLIP_SIZE = {}
+WEP_CLIP_SIZE["weapon_357"] = 6
+WEP_CLIP_SIZE["weapon_ar2"] = 30
+WEP_CLIP_SIZE["weapon_crossbow"] = 1
+WEP_CLIP_SIZE["weapon_pistol"] = 18
+WEP_CLIP_SIZE["weapon_rpg"] = 3
+WEP_CLIP_SIZE["weapon_shotgun"] = 6
+WEP_CLIP_SIZE["weapon_smg1"] = 45
+WEP_CLIP_SIZE["weapon_frag"] = 1
+
+function GM:PlayerSpawn(ply)
+	player_manager.SetPlayerClass(ply, "player_coop")
+
+	BaseClass.PlayerSpawn(self, ply)
+end
+
+function GM:PlayerCanPickupItem(ply, item)
+	-- Limit max ammo
+	itemClass = item:GetClass()
+	if ITEM_TO_AMMO[itemClass] ~= nil then -- Item exists in conversion table
+		ammo = ITEM_TO_AMMO[itemClass]
+		maxAmmo = MAX_AMMO[ammo]
+		itemAmmo = ITEM_TO_AMOUNT[itemClass]
+		currentAmmo = ply:GetAmmoCount(ammo)
+		if currentAmmo >= maxAmmo then
+			return false
+		elseif currentAmmo + itemAmmo > maxAmmo then
+			ply:SetAmmo(maxAmmo, ammo)
+		end
+	end
+	
+	return true
+end
+
+function GM:PlayerCanPickupWeapon(ply, wep)
+	wepClass = wep:GetClass()
+	if !ply:HasWeapon(wepClass) then
+		return true
+	end
+	
+	if WEP_TO_AMMO[wepClass] ~= nil then -- Weapon exists in conversion table
+		ammo = WEP_TO_AMMO[wepClass]
+		maxAmmo = MAX_AMMO[ammo]
+		clipSize = WEP_CLIP_SIZE[wepClass]
+		currentAmmo = ply:GetAmmoCount(ammo)
+		if currentAmmo >= maxAmmo then
+			return false
+		elseif currentAmmo + clipSize > maxAmmo then
+			ply:SetAmmo(maxAmmo, ammo)
+		end
+	end
+
+	return true
+end
---
+++ gamemode/shared.lua
@@ -0,0 +1,10 @@
+include("player_class/player_coop.lua")
+
+GM.Name = "Coop"
+GM.Author = "N/A"
+GM.Email = "N/A"
+GM.Website = "N/A"
+
+function GM:Initialize()
+
+end 
