7aa031da56818198702a2c69ad1d1bc871c2a451
Panzerhandschuh <TylerBeff@gmail.com>
2014-06-12 00:22:17 -0400

    Removed more spammable buttons in metamorphics

--- gamemode/mapfixes/coop_metamorphics_egypt_level1.lua
+++ gamemode/mapfixes/coop_metamorphics_egypt_level1.lua
@@ -1,3 +1,6 @@
 HOOKS["InitPostEntity"] = function()
 	ents.FindByName("b2")[1]:Remove()
+	ents.FindByName("b3zombie")[1]:Remove()
+	ents.FindByName("b3ittybuttontopit_2")[1]:Remove()
+	ents.FindByName("b3ittybuttontopit_3")[1]:Remove()
 end
