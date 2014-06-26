12a5daee501abfd597d811df084b2b869964d13f
Panzerhandschuh <TylerBeff@gmail.com>
2014-06-25 22:59:55 -0400

    synb2_diehard potential fix

--- gamemode/mapfixes/synb2_diehard.lua
+++ gamemode/mapfixes/synb2_diehard.lua
@@ -5,4 +5,7 @@ HOOKS["EntityKeyValue"] = function(e,k,v)
 	if(e:EntIndex() == 275 && k == "spawnflags") then
 		return "1024"
 	end
+	if(e:GetName() == "man1" && k == "spawnflags") then
+		return "3840"
+	end
 end
