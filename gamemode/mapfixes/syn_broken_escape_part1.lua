b06f5583e069a6ea91207d8ef5cc410f3693bd6f
Panzerhandschuh <TylerBeff@gmail.com>
2014-07-17 20:39:27 -0400

    Adjusted fast zombie spawners

---
+++ gamemode/mapfixes/syn_broken_escape_part1.lua
@@ -0,0 +1,9 @@
+HOOKS["InitPostEntity"] = function()
+	local ent = ents.FindByName("zombine2_spawner")[1]
+	ent:SetKeyValue("SpawnFrequency", "1")
+	ent:SetKeyValue("MaxNPCCount", "15")
+	
+	local ent = ents.FindByName("fastzombie3_maker")[1]
+	ent:SetKeyValue("SpawnFrequency", "1")
+	ent:SetKeyValue("MaxNPCCount", "15")
+end
