76358e7799b166d596d617499d955424bfc7c6bb
Panzerhandschuh <TylerBeff@gmail.com>
2014-07-24 15:19:06 -0400

    Removed areaportal from likeside_village

--- gamemode/mapfixes/lakeside_village.lua
+++ gamemode/mapfixes/lakeside_village.lua
@@ -1,5 +1,6 @@
 HOOKS["InitPostEntity"] = function()
 	ents.FindByName("pesula_portal3")[1]:Remove()
+	ents.FindByName("pesula_portal2")[1]:Remove()
 	ents.FindByName("ff_door1")[1]:Remove()
 	
 	local ge = ents.Create("game_end")
