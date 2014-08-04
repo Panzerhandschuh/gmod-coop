47fa146785e8d6e93444fcbb58b4c01aed1522b7
Panzerhandschuh <TylerBeff@gmail.com>
2014-08-04 04:29:13 -0400

    Added spawn weapons to syn_bunker

--- gamemode/mapfixes/syn_bunker.lua
+++ gamemode/mapfixes/syn_bunker.lua
@@ -1,3 +1,13 @@
+-- Adjust weapon loadout
+HOOKS["PlayerSpawn"] = function(ply)
+	timer.Simple( 0.1, function()
+		ply:Give("weapon_shotgun")
+
+		ply:GiveAmmo(225, "smg1", true)
+		ply:GiveAmmo(30, "buckshot", true)
+	end )
+end
+
 HOOKS["InitPostEntity"] = function()
 	local cp = ents.Create("coop_checkpoint")
 	cp:SetPos(Vector(56, 2648, -88))
