db8d990b70e4b43e8c7ec4e70555b4257f21cf82
Panzerhandschuh <TylerBeff@gmail.com>
2014-08-05 17:16:08 -0400

    Added game_end to coop_blackmesa

--- gamemode/mapfixes/coop_blackmesa_episode2.lua
+++ gamemode/mapfixes/coop_blackmesa_episode2.lua
@@ -8,4 +8,13 @@ HOOKS["InitPostEntity"] = function()
 	for k,v in pairs(ents.FindByName("lift")) do
 		v:SetKeyValue("speed", "400")
 	end
+	
+	local ge = ents.Create("game_end")
+	ge:SetName("fakegameender")
+	ge:Spawn()
+	for k,v in pairs(ents.FindByClass("func_button")) do
+		if(v:GetPos() == Vector(1628, 393.5, -3222)) then
+			v:Fire("AddOutput","OnPressed fakegameender,EndGame,,5,1",0)
+		end
+	end
 end
