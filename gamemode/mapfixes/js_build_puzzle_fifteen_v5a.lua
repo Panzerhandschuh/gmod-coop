72e2471c11c7a0379242cc99b457cbbbbc559f5a
Panzerhandschuh <TylerBeff@gmail.com>
2014-08-05 19:46:37 -0400

    Removed spawn kill trigger on puzzle_fifteen

--- gamemode/mapfixes/js_build_puzzle_fifteen_v5a.lua
+++ gamemode/mapfixes/js_build_puzzle_fifteen_v5a.lua
@@ -1,4 +1,10 @@
 HOOKS["InitPostEntity"] = function()
 	ents.FindByName("painful_gun")[1]:Remove()
 	ents.FindByName("trigger_gun")[1]:Remove()
+	
+	for k,v in pairs(ents.FindByClass("trigger_hurt")) do
+		if(v:GetPos() == Vector(-4306.07, 4284.99, 198.5)) then
+			v:Remove()
+		end
+	end
 end
