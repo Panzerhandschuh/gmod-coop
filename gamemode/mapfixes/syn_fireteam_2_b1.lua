fb9c4979603c35eb4583d9b86dd9a007e0ea1a5c
Panzerhandschuh <TylerBeff@gmail.com>
2014-08-07 04:43:04 -0400

    Spawn weapons + jeep for syn_fireteam_2

---
+++ gamemode/mapfixes/syn_fireteam_2_b1.lua
@@ -0,0 +1,20 @@
+-- Adjust weapon loadout
+HOOKS["PlayerSpawn"] = function(ply)
+	timer.Simple( 0.1, function()
+		ply:Give("weapon_shotgun")
+		ply:Give("weapon_rpg")
+
+		ply:GiveAmmo(225, "smg1", true)
+		ply:GiveAmmo(30, "buckshot", true)
+	end )
+end
+
+HOOKS["InitPostEntity"] = function()
+	local s = ents.Create("vehicle_spawner")
+	s:SetPos(Vector(-6400, -10368, 64.95))
+	s:SetAngles(Angle(0, 270, 0))
+	s.jmodel = "models/buggy.mdl"
+	s.jscript = "scripts/vehicles/jeep_test.txt"
+	s.jeep = true
+	s:Spawn()
+end
