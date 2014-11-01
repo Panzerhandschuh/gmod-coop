82aac3177f545742c288c2546bb07afc6837db16
Panzerhandschuh <TylerBeff@gmail.com>
2014-10-31 23:00:51 -0400

    Added ammo to player_equip on oc_sso

--- gamemode/mapfixes/oc_sso_v1j.lua
+++ gamemode/mapfixes/oc_sso_v1j.lua
@@ -3,3 +3,12 @@ HOOKS["EntityKeyValue"] = function(e,k,v)
 		return "sprites/strider_blackball.spr"
 	end
 end
+
+HOOKS["InitPostEntity"] = function()
+	ents.FindByName("breakmetaldoor")[1]:Remove()
+	
+	local eq1 = ents.FindByClass("eq_spawnLv1")[1]
+	eq1:SetKeyValue("item_ammo_357_large", "1", 0)
+	eq1:SetKeyValue("item_ammo_crossbow", "10", 0)
+	eq1:SetKeyValue("item_box_buckshot", "4", 0)
+end
