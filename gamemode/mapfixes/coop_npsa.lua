cab3fdd2d0b785c6d2accd765c7b591dc5dda125
Panzerhandschuh <TylerBeff@gmail.com>
2015-01-20 20:17:47 -0500

    Fixed npsa crash, added alpha to stamina bars

--- gamemode/mapfixes/coop_npsa.lua
+++ gamemode/mapfixes/coop_npsa.lua
@@ -1,5 +1,5 @@
--- Adjust spawners to be more fun
 HOOKS["EntityKeyValue"] = function(e,k,v)
+	-- Adjust spawners to be more fun
 	if (e:GetClass() == "npc_template_maker" || e:GetClass() == "npc_maker") then
 		if (k == "MaxNPCCount") then
 			return math.Round(v * 2)
@@ -11,13 +11,16 @@ HOOKS["EntityKeyValue"] = function(e,k,v)
 	if (k == "OnBreak" && v == "rpg_player_block,Open,,0,-1") then
 		return "!activator,AddOutput,dummykey 0,0,-1"
 	end
+end
 	
+HOOKS["InitPostEntity"] = function()
 	local ge = ents.Create("game_end")
 	ge:SetName("fakegameender")
 	ge:Spawn()
 	for k,v in pairs(ents.FindByClass("trigger_once")) do
 		if(v:GetPos() == Vector(7616, -2816, -88)) then
 			v:Fire("AddOutput","OnStartTouch fakegameender,EndGame,,10,-1",0)
+			break
 		end
 	end
 end
