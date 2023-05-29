3ca2111224869c2519e1c50aa606e882ed787af6
Tyler Befferman <TylerBeff@gmail.com>
2023-05-29 09:22:40 -0600

    Added pointshop command and discord to adverts

--- gamemode/sv_adverts.lua
+++ gamemode/sv_adverts.lua
@@ -2,9 +2,10 @@ local messages = {
 	"Type 'rtv' if you want to change the map.",
 	"Type 'nominate' if you want to choose a map to appear in the next vote.",
 	"Type '!spectate' to spectate other players. You can rejoin the team by typing the command again.",
+	"Type '!shop' or press F3 to access the points shop.",
 	"Teleport to teamates by typing '!tpto <player name>'.",
 	"If you are missing models and textures, try mounting Half-Life: Source, Half-Life 2: Episode 1, and Half-Life 2: Episode 2.",
-	"Join our group at http://steamcommunity.com/groups/gmodcoop to discuss the server, leave suggestions, or report bugs."
+	"Join our group at http://steamcommunity.com/groups/gmodcoop and https://discord.gg/3RbpsDpXbj to discuss the server, leave suggestions, or report bugs."
 }
 
 timer.Create("Advertising",120,0,function()
