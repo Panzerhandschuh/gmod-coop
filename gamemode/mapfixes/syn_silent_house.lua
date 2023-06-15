f1d983b9fa42184054b8e809013a9310f38ad3ad
Tyler Befferman <TylerBeff@gmail.com>
2023-06-14 23:41:27 -0600

    Adjusted NPC count

--- gamemode/mapfixes/syn_silent_house.lua
+++ gamemode/mapfixes/syn_silent_house.lua
@@ -26,9 +26,9 @@ HOOKS["EntityKeyValue"] = function(e,k,v)
 
 	if (e:GetClass() == "npc_template_maker" || e:GetClass() == "npc_maker") then
 		if (k == "MaxNPCCount") then
-			return math.Round(v * 8)
+			return math.Round(v * 6)
 		elseif (k == "MaxLiveChildren") then
-			return math.Round(v * 4)
+			return math.Round(v * 3)
 		end
 	end
 end
