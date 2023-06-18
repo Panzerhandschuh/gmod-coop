149f030ddf4d49ce0363d3eaeff3043c5cdf2234
Tyler Befferman <TylerBeff@gmail.com>
2023-06-18 10:06:12 -0600

    Map fixes

--- gamemode/mapfixes/syn_fogisland.lua
+++ gamemode/mapfixes/syn_fogisland.lua
@@ -10,6 +10,32 @@ HOOKS["InitPostEntity"] = function()
 		v:SetKeyValue("target","")
 		v:Spawn()
 	end
+
+	for k,v in pairs(ents.FindByName("gonarchreleasebutton")) do
+		v:Fire("AddOutput","OnPressed house2_basementdoor01,Unlock,,0,-1",0)
+		v:Fire("AddOutput","OnPressed house2_basementdoor01,Open,,1,-1",0)
+		v:Fire("AddOutput","OnPressed pzombie01_trigger,Enable,,0,-1",0)
+	end
+
+	for k,v in pairs(ents.FindByName("house2_basementdoor01_poleclip")) do
+		v:Remove()
+	end
+
+	for k,v in pairs(ents.FindByName("house2_basementdoor01_clip")) do
+		v:Remove()
+	end
+
+	for k,v in pairs(ents.FindByName("h2basement_counter")) do
+		v:SetKeyValue("max", "20")
+	end
+
+	for k,v in pairs(ents.FindByName("elite_counter")) do
+		v:SetKeyValue("max", "12")
+	end
+
+	for k,v in pairs(ents.FindByName("Strider01_timertofire")) do
+		v:SetKeyValue("RefireTime", "32")
+	end
 end
 
 HOOKS["EntityKeyValue"] = function(e,k,v)
