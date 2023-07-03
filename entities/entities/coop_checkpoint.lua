8aa37e5bd731db416cb80377742307bfb91ff699
Tyler Befferman <tylerbeff@gmail.com>
2023-07-03 11:39:51 -0600

    Added activation radius to coop_checkpoint

--- entities/entities/coop_checkpoint.lua
+++ entities/entities/coop_checkpoint.lua
@@ -1,7 +1,41 @@
 ENT.Type = "point"
 
+function ENT:KeyValue(key, value)
+	if (!self.keyvalues) then
+		self.keyvalues = {}
+	end
+	
+	key = string.lower(key)
+	self.keyvalues[key] = value
+end
+
 function ENT:AcceptInput(inputName, activator, called, data)
 	if(inputName == "SetAsCP") then
 		GAMEMODE.currentspawn = self
+		self.hasActivated = true
+	end
+end
+
+function ENT:Think()
+	if (!self.keyvalues["radius"] || self.hasActivated) then
+		return
+	end
+
+	for _, ply in pairs(player.GetAll()) do
+		local radius = tonumber(self.keyvalues["radius"])
+		if (ply:GetPos():Distance(self:GetPos()) < radius) then
+			if (self.keyvalues["updatecp"]) then
+				GAMEMODE.currentspawn = self
+			end
+			if (self.keyvalues["moveplayers"]) then
+				for _, ply2 in pairs(player.GetAll()) do
+					if (ply != ply2) then
+						ply2:SetPos(self:GetPos())
+					end
+				end
+			end
+
+			self.hasActivated = true
+		end
 	end
 end
