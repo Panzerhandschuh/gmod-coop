6dec50196fd678be83572b9b9c93075cefc4b6e2
Tyler Befferman <tylerbeff@gmail.com>
2023-07-08 22:00:41 -0600

    Fixed invisible zombies Added more zombies

--- gamemode/mapfixes/oc_waking_dead.lua
+++ gamemode/mapfixes/oc_waking_dead.lua
@@ -4,6 +4,17 @@ HOOKS["InitPostEntity"] = function()
 			v:SetKeyValue("message", "so it'll take about 5 minutes to get there.")
 		end
 	end
+	
+	for k,v in pairs(ents.FindByClass("npc_*")) do
+		if (string.find(v:GetClass(), "maker")) then
+			continue
+		end
+
+		v:SetKeyValue("model", "")
+		v:SetKeyValue("gibmodel0", "")
+		v:SetKeyValue("gibmodel1", "")
+		v:SetKeyValue("gibmodel2", "")
+	end
 end
 
 HOOKS["EntityKeyValue"] = function(e,k,v)
@@ -11,5 +22,13 @@ HOOKS["EntityKeyValue"] = function(e,k,v)
 		local split = string.Split(v, ",")
 		local val = tonumber(split[4]) / 2 -- Half the output delay
 		return string.gsub(v, split[4], tostring(val))
+	end
+
+	if (e:GetClass() == "npc_template_maker") then
+		if (k == "SpawnFrequency") then
+			return math.Round(v * 0.5)
+		elseif (k == "MaxLiveChildren") then
+			return math.Round(v * 4)
+		end
 	end
 end
