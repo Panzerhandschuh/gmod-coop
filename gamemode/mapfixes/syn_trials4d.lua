91894660b652b72ba21d2f97518ec1b28947f50c
Tyler Befferman <diabolicremedy@gmail.com>
2024-02-26 12:24:53 -0700

    Fixed truck script

--- gamemode/mapfixes/syn_trials4d.lua
+++ gamemode/mapfixes/syn_trials4d.lua
@@ -2,6 +2,6 @@ HOOKS["InitPostEntity"] = function()
 	local coopTrigger = ents.FindByName("trial_a_hud")[1]
 	coopTrigger:SetKeyValue("PlayerValue", "50")
 	
-	local tr = SYN_V.CreateTruck(Vector(2548, 423.68, -442.97),Angle(0, 270, 0),"scripts/vehicles/trialstruck.txt")
+	local tr = SYN_V.CreateTruck(Vector(2548, 423.68, -442.97),Angle(0, 270, 0),"scripts/vehicles/truck.txt")
 	tr:SetName("trial_e_truck1") --the map requires this
 end
